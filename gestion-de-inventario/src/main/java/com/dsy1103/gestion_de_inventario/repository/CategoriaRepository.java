7da65d646e4c146a0907030cbdb98d5c5cf2ab8b
goncalderon2 <gon.calderon@duocuc.cl>
2026-05-16 21:17:53 -0400

    Delete gestion-de-inventario directory

--- gestion-de-inventario/src/main/java/com/dsy1103/gestion_de_inventario/repository/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dsy1103.gestion_de_inventario.repository;
-
-import com.dsy1103.gestion_de_inventario.model.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
